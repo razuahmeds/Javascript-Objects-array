4929d4df940ec1ea4f62434b92b6de0d30d5e9fc
razuahmeds <rukunuzzaman.razu@gmail.com>
2023-06-17 15:54:41 +0600

    commit

--- Objects.js
+++ Objects.js
@@ -26,7 +26,7 @@ const Age = {
 
 
 // nested object
-const student = { 
+const student2 = { 
     name: 'RAZU', 
     age: 18/3,
     marks: {
@@ -41,11 +41,60 @@ const student = {
 //console.log(student.marks.science); 
 
 
-const person = {
+const person22 = {
     name: 'Sam',
     age: 30,
     
     greet: function() { console.log('HI') }
 }
 
-person.greet();
+//person.greet();
+
+
+
+
+const student = { 
+    name: 'mmmm', 
+    age: 20,
+    marks: {
+        science: 'razu',
+        math: 75
+    }
+}
+
+
+//console.log(student.marks);
+
+
+//console.log(student.marks.science);
+
+
+const person1 = {
+    name: 'John',
+    greet: function() { console.log('hello','hello','hello','hello',111); }
+};
+
+//person.name;
+
+//person.greet(); 
+
+
+const person = {
+    name: 'RAZU',
+    age: 18,
+
+
+    greet: function() { console.log('The age is' + ' ' + this.age); }
+};
+
+//person.greet();
+
+const text = {
+    name: 'razu',
+    age: 30,
+    greet: function() {
+        let surname = 'Doe';
+        console.log('The name is' + ' ' + this.name + ' ' + surname); }
+};
+
+text.greet();
