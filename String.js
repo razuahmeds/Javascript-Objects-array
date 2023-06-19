0d81377cd3b6a5c3ca01b7adf3c77b6ad3b2e50d
razuahmeds <rukunuzzaman.razu@gmail.com>
2023-06-19 12:32:35 +0600

    commit

---
+++ String.js
@@ -0,0 +1,50 @@
+const text1 = 'hello';
+const text2 = 'world';
+const text3 = '     razu    ';
+
+
+
+const result1 = text1.concat(' ', text2);
+//console.log(result1); 
+
+
+const result2 = text1.toUpperCase();
+//console.log(result2);
+
+const result3 = text3.trim();
+//console.log(result3); 
+
+
+const result4 = text1.split();
+//console.log(result4); 
+
+
+const result5= text1.slice(1, 3);
+//console.log(result5); 
+
+
+
+
+
+const a = 'razu'; 
+const b = 22222; 
+
+
+const resultn1 = String(a);
+const resultn2 = String(b);
+
+//console.log(resultn1);
+//console.log(resultn2); 
+
+
+
+
+const number1 = 0/5;
+console.log(number1); 
+
+const number2 = -0/2;
+console.log(number2); 
+
+// strings can't be divided by numbers
+const number3 = "abcdef"/5; 
+console.log(number3);  
